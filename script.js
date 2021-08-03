54b59b9957085a0b5869fb1d2e795ee1345ecbbc
niranj775 <niranj775@gmail.com>
2021-08-03 18:26:34 +0530

    03-08-2021

--- script.js
+++ script.js
@@ -2,25 +2,27 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en_US/";
 
 async function fetchValue() {
   let value = document.querySelector("input").value;
-  const apiUrl = url + value;
-  let response = await fetch(apiUrl);
-  let data = await response.json();
-  console.log(data);
-  let ans = [];
-  data[0].meanings[0].definitions.forEach((ele) => ans.push(ele.definition));
-  console.log(ans);
+  if (value == "") alert("Enter a word");
+  else {
+    const apiUrl = url + value;
+    let response = await fetch(apiUrl);
+    let data = await response.json();
+    console.log(data);
+    let ans = [];
+    data[0].meanings[0].definitions.forEach((ele) => ans.push(ele.definition));
 
-  let input = document.querySelector("input");
-  input.value = "";
+    let input = document.querySelector("input");
+    input.value = "";
 
-  let meaning = document.getElementById("meaning");
-  meaning.innerHTML = "";
+    let meaning = document.getElementById("meaning");
+    meaning.innerHTML = "";
 
-  ans.forEach((ele) => {
-    let meanRow = document.createElement("li");
-    meanRow.innerHTML = ele;
+    ans.forEach((ele) => {
+      let meanRow = document.createElement("li");
+      meanRow.innerHTML = ele;
 
-    let newRow = document.getElementById("meaning");
-    newRow.append(meanRow);
-  });
+      let newRow = document.getElementById("meaning");
+      newRow.append(meanRow);
+    });
+  }
 }
