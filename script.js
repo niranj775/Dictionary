e133907e4060b5848c77d3a06c83bd053125037a
niranj775 <niranj775@gmail.com>
2021-08-03 18:27:55 +0530

    03-08-2021

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function fetchValue() {
     const apiUrl = url + value;
     let response = await fetch(apiUrl);
     let data = await response.json();
-    console.log(data);
+
     let ans = [];
     data[0].meanings[0].definitions.forEach((ele) => ans.push(ele.definition));
 
